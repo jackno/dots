c9316359ed9272179c8c4c348fb685b7d0e2aa6e
jackno <jacknovotny8@gmail.com>
2020-07-01 11:52:53 -0700

    make rc additions prettier

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ if [ ! -x "$(command -v brew)" ]; then
 
     echo 'Adding homebrew to your path'
     if [ -w ~/.bashrc ]; then
+        echo '' >> ~/.bashrc
         echo '# Add brew to the path' >> ~/.bashrc
         echo 'export PATH=$HOME/.brew/bin:$PATH' >> ~/.bashrc
     fi
     if [ -w ~/.zshrc ]; then
+        echo '' >> ~/.zshrc
         echo '# Add brew to the path' >> ~/.zshrc
         echo 'export PATH=$HOME/.brew/bin:$PATH' >> ~/.bashrc
     fi
@@ -28,12 +30,14 @@ if [ -x "$(command -v cargo)" ]; then
 
     echo 'Adding Rust to your path'
     if [ -w ~/.bashrc ]; then
+        echo '' >> ~/.bashrc
         echo '# Add rust to the path' >> ~/.bashrc
         echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc
     fi
     if [ -w ~/.zshrc ]; then
+        echo '' >> ~/.zshrc
         echo '# Add rust to the path' >> ~/.zshrc
-        echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc
+        echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.zshrc
     fi
 fi
 
@@ -41,11 +45,12 @@ echo 'Installing crates from .crates'
 cat ./.crates | xargs ~/.cargo/bin/cargo install
 
 if [ ! -e ~/repos/zsh-syntax-highlighting ]; then
-    echo "Installing zsh-syntax-highlighting..."
+    echo 'Installing zsh-syntax-highlighting...'
     mkdir -p ~/repos
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/repos/zsh-syntax-highlighting
-    echo "# Enable syntax highlighting" >> ~/.zshrc
-    echo "source ~/repos/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+    echo '' >> ~/.zshrc
+    echo '# Enable syntax highlighting' >> ~/.zshrc
+    echo 'source ~/repos/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
 fi
 
 echo 'To use the installed tools, restart your terminal or do:'
